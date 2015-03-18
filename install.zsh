c05aa8d77e5206c27ccee0beb0f675c00f4cb17a
xcv58 <chenyihonglove@gmail.com>
2015-03-18 13:03:04 -0400

    add install.zsh to link all rcfiles

---
+++ install.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  echo ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
