156453905d5a7eef4c2d2b95c817ef0ac295672d
xcv58 <i@xcv58.com>
2016-08-25 17:30:21 -0700

    add lazy load to speed up startup time

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -7,28 +7,32 @@
 #
 
 # Load manually installed NVM into the shell session.
-if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-  source "$HOME/.nvm/nvm.sh"
+function load_nvm() {
+    if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
+        source "$HOME/.nvm/nvm.sh"
 
-# Load package manager installed NVM into the shell session.
-elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nvm 2>/dev/null)" ]]; then
-  source $(brew --prefix nvm)/nvm.sh
+    # Load package manager installed NVM into the shell session.
+    elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nvm 2>/dev/null)" ]]; then
+        source $(brew --prefix nvm)/nvm.sh
 
-# Return if requirements are not found.
-elif (( ! $+commands[node] )); then
-  return 1
-fi
+    # Return if requirements are not found.
+    elif (( ! $+commands[node] )); then
+        return 1
+    fi
 
-# Load NPM completion.
-if (( $+commands[npm] )); then
-  cache_file="${0:h}/cache.zsh"
+    # Load NPM completion.
+    if (( $+commands[npm] )); then
+        cache_file="${0:h}/cache.zsh"
 
-  if [[ "$commands[npm]" -nt "$cache_file" || ! -s "$cache_file" ]]; then
-    # npm is slow; cache its output.
-    npm completion >! "$cache_file" 2> /dev/null
-  fi
+        if [[ "$commands[npm]" -nt "$cache_file" || ! -s "$cache_file" ]]; then
+            # npm is slow; cache its output.
+            npm completion >! "$cache_file" 2> /dev/null
+        fi
 
-  source "$cache_file"
+        source "$cache_file"
 
-  unset cache_file
-fi
+        unset cache_file
+    fi
+}
+
+lazy_load load_nvm npm node nvm
